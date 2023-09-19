15734fc6b6237469affb79b83ef08cf0984a9ecf
Cnnb01 <nyamambanyaguch@gmail.com>
2023-09-19 10:10:43 -0700

    second commit

--- task1.c
+++ task1.c
@@ -1,46 +1,33 @@
-#include "main.h"
+#include <stdarg.h>
 
-/**
- * _printint - Print out a number.
- * @args: A va_list containing the integer to be printed.
- * Return: The number of characters printed.
- */
+#include <stdio.h>
+
+/* _printint - Print out an integer.
+ *@args: A va_list containing the integer to be printed.
+
+*Return: The number of characters printed. */ 
 int _printint(va_list args)
 {
-    char buffer[10];
-    int i = 0;
-    int n = va_arg(args, int); 
-    int count = 0;
+       	int num = va_arg(args, int);
+	int count = 0;
+	int negative = num < 0;
 
-    if (n < 0)
-    {
-        putchar('-');
-        count++;
-        n = -n;
-    }
-
-    if (n == 0)
-    {
-        putchar('0'); 
-        return (1); 
-    }
-
-    
-    while (n > 0)
-    {
-        buffer[i] = (n % 10) + '0'; 
-        n /= 10;
-        i++;
-    }
-
-   
-    while (i > 0)
-    {
-        i--;
-        putchar(buffer[i]);
-        count++;
-    }
-
-    return (count);
+if (negative)
+{
+       	num = -num;
+       	count += putchar('-');
 }
 
+if (num == 0) 
+{ 
+	count += putchar('0');
+       	return count;
+}
+
+while (num > 0)
+{
+       	count += putchar((num % 10) + '0');
+       	num = num / 10; }
+
+return (count);
+}
