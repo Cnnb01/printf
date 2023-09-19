d994039cfc802205f8b0f7fec4cbe00e0e664ff2
Smasher2198 <nickysmasher2198@gmail.com>
2023-09-19 15:32:06 -0700

    second commit

--- task1.c
+++ task1.c
@@ -1,48 +1,50 @@
 #include "main.h"
-/**
- * pnumber - Rcursively prints out an integer.
- * @dig: the integer to be printed.
- *
- * description: function that recursively print an integer 'dig'.
- * if 'dig' is equal to INT_MIN it handle a special case for INT_MIN.
- * return: number of char printed.
-*/
-int pnumber(int dig)
-{
-int count = 0;
-if (dig == INT_MIN)
-{
-putchar('-');
-count++;
-count += pnumber(INT_MAX / 10);
-putchar(INT_MAX % 10 + 1 + '0');
-count++;
-return (count);
-}
-else if (dig < 0)
-{
-putchar('-');
-count++;
-dig = -dig;
-}
-if (dig >= 10)
-{
-count += pnumber(dig / 10);
-}
-putchar(dig % 10 + '0');
-count++;
-return (count);
-}
-
-/**
- * _printint - print out integer using pnumber.
- * @args: va_list containing number of integers to be printed.
- *
- * description: the function serves as a wrapper for the pnumber function.
- * return: number of characters printed.
- */
-int _printint(va_list args)
-{
-int numb = va_arg(args, int);
-return (pnumber(numb));
-}
+  2
+  3 /**
+  4 * pnumber - Recursively print out a number.
+  5 * @dig: The integer to be printed.
+  6 *
+  7 * Return: The number of characters printed.
+  8 */
+  9 int pnumber(int dig)
+ 10 {
+ 11 int count = 0;
+ 12
+ 13 if (dig == INT_MIN)
+ 14 {
+ 15 putchar('-');
+ 16 count++;
+ 17 count += pnumber(INT_MAX / 10);
+ 18 putchar(INT_MAX % 10 + 1 + '0');
+ 19 count++;
+ 20 return (count);
+ 21 }
+ 22 else if (dig < 0)
+ 23 {
+ 24 putchar('-');
+ 25 count++;
+ 26 dig = -dig;
+ 27 }
+ 28
+ 29 if (dig >= 10)
+ 30 {
+ 31 count += pnumber(dig / 10);
+ 32 }
+ 33
+ 34 putchar(dig % 10 + '0');
+ 35 count++;
+ 36
+ 37 return (count);return (count);
+ 38 }
+ 39
+ 40 /**
+ 41 * _printint - Print an integer using pnumber.
+ 42 * @args: The arguments list.
+ 43 *
+ 44 * Return: The number of characters printed.
+ 45 */
+ 46 int _printint(va_list args)
+ 47 {
+ 48 int numb = va_arg(args, int);
+ 49 return (pnumber(numb));
+ 50 }
