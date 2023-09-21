b9ef59bd064f318b623b2fa62411f2dd9114f757
Cnnb01 <nyamambanyaguch@gmail.com>
2023-09-21 10:14:12 -0700

    binary

--- task2.c
+++ task2.c
@@ -10,12 +10,25 @@ int print_binary(va_list args)
 unsigned int num = va_arg(args, unsigned int);
 int count = 0;
 int bit_position = sizeof(unsigned int) * 8 - 1;
+int leading_zeroes = 1;
 while (bit_position >= 0)
 {
 char bit = (num & (1 << bit_position)) ? '1' : '0'; 
+if (bit == '1')
+{
+leading_zeroes = 0;
+}
+if (!leading_zeroes || bit_position == 0)
+{
 write(1, &bit, 1);
 count++;
+}
 bit_position--;
+}
+if (count == 0)
+{
+write(1, "0", 1);
+count = 1;
 }
 return (count);
 }
